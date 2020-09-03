9de943b9705fb619d9cc9435ef0b2480390fdba0
Rodrigo Silva <rodrigo.silva5@wipro.com>
2020-09-02 23:48:51 -0300

    363. Sending Requests & Responses and Working with Postman

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feeds');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use('/feed', feedRoutes);
 
--- routes/feeds.js
+++ routes/feeds.js
@@ -4,5 +4,6 @@ const feedController = require('../controllers/feeds');
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
+router.post('/post', feedController.createPost);
 
 module.exports = router;
